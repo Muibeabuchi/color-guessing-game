2d6f7362b3e4057a2587c574028bec48dfc2bda9
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-02-03 16:12:09 +0100

    add tailwind-css + fix height of app component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-red-500 h-[100vh]">
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
