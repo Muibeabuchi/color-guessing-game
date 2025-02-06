0825c99db97ea21c41bd14d4ad3e426ff3ee508e
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-02-06 22:33:26 +0100

    add data-testid to score component

--- src/components/score.tsx
+++ src/components/score.tsx
@@ -3,7 +3,7 @@ import { useColor } from "../hooks/use-color-hooks";
 const Score = () => {
   const { state } = useColor();
   return (
-    <div className="text-2xl font-bold text-center mb-6">
+    <div className="text-2xl font-bold text-center mb-6" data-testid="score">
       Score: <span data-testid="score">{state.score}</span>
     </div>
   );
